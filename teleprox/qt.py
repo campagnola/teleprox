5e7b02b44ca6053908bcbacb3c6d25664b6fcc1c
Luke Campagnola <luke.campagnola@gmail.com>
2025-01-27 16:39:21 -0800

    add qt6 support, also check for known qt6 bug

--- teleprox/qt.py
+++ teleprox/qt.py
@@ -1,15 +1,44 @@
+import sys
+import importlib
 
-try:
-    from PyQt5.QtCore import *
-    from PyQt5.QtGui import *
-    from PyQt5.QtWidgets import *
-    HAVE_QT = True
-except ImportError:
-    HAVE_QT = False
+
+def import_qt_namespace(qt_lib):
+    ns = {}
+    qtcore = importlib.import_module(qt_lib + '.QtCore')
+    ns.update(qtcore.__dict__)
+    qtgui = importlib.import_module(qt_lib + '.QtGui')
+    ns.update(qtgui.__dict__)
+    qtwidgets = importlib.import_module(qt_lib + '.QtWidgets')
+    ns.update(qtwidgets.__dict__)
+    return ns
+
+
+HAVE_QT = False
+qt_libs = ['PyQt6', 'PySide6', 'PyQt5', 'PySide2']
+
+# already imported?
+for qt_lib in qt_libs:
+    if qt_lib in sys.modules:
+        HAVE_QT = True
+        QT_LIB = qt_lib
+        break
+
+# import first available
+for qt_lib in qt_libs:
+    try:
+        importlib.import_module(qt_lib)
+        HAVE_QT = True
+        QT_LIB = qt_lib
+        break
+    except ImportError:
+        pass
 
 
 if HAVE_QT:
-    Signal = pyqtSignal  # for compatibility with PySide
+    locals().update(import_qt_namespace(QT_LIB))
+    
+    if 'PySide' not in QT_LIB:
+        Signal = pyqtSignal  # for compatibility with PySide
 
     def make_qapp():
         """Create a QApplication object if one does not already exist.
--- teleprox/tests/check_qt.py
+++ teleprox/tests/check_qt.py
@@ -5,8 +5,8 @@ import teleprox
 qt_available = True
 qt_reason = ""
 
+p = teleprox.start_process('check_qt_process')
 try:    
-    p = teleprox.start_process()
     qt = p.client._import('teleprox.qt')
     try:
         app = qt.QApplication([], _timeout=1)
@@ -19,5 +19,16 @@ try:
 except ImportError as exc:
     qt_available = False
     qt_reason = str(exc)
+
+finally:
+    p.client.close_server()
+    for i in range(10):
+        if p.poll() is not None:
+            break
+    if p.poll() is None:
+        # indicates presence of deadlock; see failure_modes.py exit_deadlock_qt6
+        qt_available = False
+        qt_reason = "Known exit deadlock in Qt would break tests"
+        p.kill()
 
 requires_qt = pytest.mark.skipif(not qt_available, reason=qt_reason)
