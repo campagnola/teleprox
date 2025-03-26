f360d77c4006d382bc94f9081b27a18481f4c955
Martin Chase <outofculture@gmail.com>
2025-03-26 13:05:00 -0700

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ packages = [x for x in find_packages('.') if x.startswith('teleprox')]
 
 setup(
     name = "teleprox",
-    version = "1.0",
+    version = "1.0.1",
     author = "Luke Campagnola and Samuel Garcia",
     author_email = "lukec@alleninstitute.org",
     description = ("Object proxies over TCP"),
